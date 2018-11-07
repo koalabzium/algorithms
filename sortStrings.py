5eb74d7395852cde793724fd8e9bae0fe8912476
Zosia <owsiak@student.agh.edu.pl>
2018-11-08 00:18:32 +0100

    Pretty print

--- sortStrings.py
+++ sortStrings.py
@@ -1,28 +1,19 @@
-arr = ['3','30','34']
-s = '3'
-def findMaxLen(arr):
-    max = len(arr[0])
-    for i in arr:
-        len = len(i)
-        if len(i) > max:
-            max == len
-    return max
-
+arr=[]
 def cmp(a,b):
     i = 0
     a += '.'
     b += '.'
     while a[i] != '.' or b[i] != '.':
         if a[i] != '.' and b[i] == '.':
-            if a[i-1]>b[i]:
-                return -1
-            else:
+            if a[i]>b[0]:
                 return 1
+            else:
+                return -1
         if b[i]!='.' and a[i]=='.':
-            if b[i-1]>a[i]:
-                return 1
-            else:
+            if b[i]>a[0]:
                 return -1
+            else:
+                return 1
         if int(a[i]) > int(b[i]):
             return 1
         elif int(b[i]) > int(a[i]):
@@ -42,13 +33,7 @@ def cmp_to_key(mycmp):
             return mycmp(self.obj, other.obj) > 0
         def __eq__(self, other):
             return mycmp(self.obj, other.obj) == 0
-        def __le__(self, other):
-            return mycmp(self.obj, other.obj) <= 0
-        def __ge__(self, other):
-            return mycmp(self.obj, other.obj) >= 0
-        def __ne__(self, other):
-            return mycmp(self.obj, other.obj) != 0
     return K
 
 print(sorted(arr,key = cmp_to_key(cmp)))
-print(cmp('3','30'))
+print(cmp('298','29'))
