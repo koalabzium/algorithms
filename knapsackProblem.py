82e3eecaa1e68584c9a9999b281fe4e813221bdc
Zosia <owsiak@student.agh.edu.pl>
2018-12-16 21:00:10 +0100

    removed removed

--- knapsackProblem.py
+++
@@ -1,6 +0,0 @@
-weights = [1,2,4,2,5]
-values = [5,3,5,3,2]
-
-
-def knapsack(weights, values):
-    pass
--- minAbsoluteDiffrence.py
+++
@@ -1,8 +0,0 @@
-def minAbsDiff(arr):
-    for i in (0,len(arr),1):
-        print(abs(-3))
-
-
-
-a = [-1,3,5]
-minAbsDiff(a)
